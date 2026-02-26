ee23b212a73e0812c1bfab30572f8378a1f88026
tfsas <tiagobiiijuu@gmail.com>
2026-02-26 16:51:08 -0300

    NacionalitySelect

--- cypress/e2e/Login.spec.cy.js
+++ cypress/e2e/Login.spec.cy.js
@@ -8,11 +8,40 @@ describe('Treinamento de testes', () => {
       loginButton: "[type='submit']",
       sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
       dashboardGrid: ".orangehrm-dashboard-grid",
-      wrongCredentialAlert: "[role='alert']"
+      wrongCredentialAlert: "[role='alert']",
+      myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+      firstNameField: "[name='firstName']",
+      lastNameField: "[name='lastName']",
+      genericField: ".oxd-input--active",
+      dateField: "[placeholder='yyyy-dd-mm']",
+      dateCloseButton: ".--close",
+      NacionatyField: "[tabindex='0']"
+
     
     }
     
-    it('Login - Sucess', () => {
+    it.only('User Info Update - Sucess', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
+    cy.get(selectorsList.loginButton).click()
+    //cy.location('pathname').should('equal',  'web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FistName')
+    cy.get(selectorsList.lastNameField).clear().type('LastName')
+    cy.get(selectorsList.genericField).eq(4).clear().type('EmployeeId')
+    cy.get(selectorsList.genericField).eq(5).clear().type('OtherId')
+    cy.get(selectorsList.genericField).eq(6).clear().type('1234562')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2025-12-05')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.NacionatyField).eq(0).click()
+    cy.contains('Brazilian').click()
+    //cy.get(selectorsList.NacionatyField).select('20')
+
+
+  })
+  it('Login - Sucess', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
@@ -21,6 +50,7 @@ describe('Treinamento de testes', () => {
     cy.get(selectorsList.dashboardGrid)
    
   })
+
   it('Login - Fail', () => {
     cy.visit('/auth/login')  
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
---
+++ cypress/e2e/adilson.spec.cy.js
@@ -0,0 +1,6 @@
+describe('Treinamento de testes', () => {
+  it('OrangeHrm', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    
+  }) 
+})
