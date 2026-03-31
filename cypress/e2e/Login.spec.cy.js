fe60c106a6a4a6ec61e4048fcb03c49090566e67
Tfsas <tiagobiiijuu@gmil.com>
2026-03-31 19:28:23 -0300

    Successfully Updated

--- cypress/e2e/Login.spec.cy.js
+++ cypress/e2e/Login.spec.cy.js
@@ -15,10 +15,11 @@ describe('Treinamento de testes', () => {
       genericField: ".oxd-input--active",
       dateField: "[placeholder='yyyy-dd-mm']",
       dateCloseButton: ".--close",
-      NacionatyField: "[tabindex='0']",
-      MaritalStatus: "[tabindex='0']",
-      GenderField: ".oxd-radio-input",
-      DateOfBirthField: "[placeholder='yyyy-dd-mm']"    
+      nacionatyField: "[tabindex='0']",
+      maritalStatus: "[tabindex='0']",
+      genderField: ".oxd-radio-input",
+      dateOfBirthField: "[placeholder='yyyy-dd-mm']",
+      submitButton: ".orangehrm-left-space"
     }
     
     it.only('User Info Update - Sucess', () => {
@@ -26,24 +27,31 @@ describe('Treinamento de testes', () => {
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
-    //cy.location('pathname').should('equal',  'web/index.php/dashboard/index')
+    cy.location('pathname').should('equal',  '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
     cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('FistName')
-    cy.get(selectorsList.lastNameField).clear().type('LastName')
-    cy.get(selectorsList.genericField).eq(3).clear().type('EmployeeId')
-    cy.get(selectorsList.genericField).eq(4).clear().type('OtherId')
-    cy.get(selectorsList.genericField).eq(5).clear().type('OtherId')
+    cy.get(selectorsList.firstNameField).clear().type('Tiago')
+    cy.get(selectorsList.lastNameField).clear().type('Ferreira')
+    cy.get(selectorsList.genericField).eq(3).clear().type('ID28')
+    cy.get(selectorsList.genericField).eq(4).clear().type('GDP2J03')
+    cy.get(selectorsList.genericField).eq(5).clear().type('EEE5E65')
     cy.get(selectorsList.genericField).eq(6).clear().type('1234562')
     cy.get(selectorsList.dateField).eq(0).clear().type('2025-12-05')
     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.NacionatyField).eq(0).click()
-    cy.contains('Belgian').click()
-    cy.get(selectorsList.MaritalStatus).eq(1).click()
+    cy.get(selectorsList.nacionatyField).eq(0).click()
+    cy.contains('Brazilian').click()
+    cy.get(selectorsList.maritalStatus).eq(1).click()
     cy.contains('Married').click()
-    cy.get(selectorsList.GenderField).eq(1).click()
+    cy.get(selectorsList.genderField).eq(1).click()
     cy.contains('Male').click()
-    cy.get(selectorsList.DateOfBirthField).eq(1).clear().type('2025-12-05')
+    cy.get(selectorsList.dateOfBirthField).eq(1).clear().type('2025-12-05')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('.oxd-toast')
+    cy.get('body').should('contain', 'Successfully Updated')
+    
+   
+    
     
 
     
